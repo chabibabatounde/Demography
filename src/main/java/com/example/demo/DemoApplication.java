aacfe4fea936954400214132db32f026a2c2c804
Rodolpho BABATOUNDE <chabibabatounde@gmail.com>
2019-12-26 15:04:07 +0100

    Crud methods (Create, Read, and Delete)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,44 +1,12 @@
 package com.example.demo;
 
-import com.example.demo.model.Demography;
-import com.example.demo.repository.CustomDemographyRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @SpringBootApplication
-@RestController
-@RequestMapping("Demography")
 
 public class DemoApplication {
-
-	@Autowired
-	private CustomDemographyRepository respository;
-
-	//Method for creating a demographic event
-	//You can create a single event or several events.
-	@PostMapping("/create")
-	public List<Demography> create(@RequestBody List<Demography> demographies) {
-		respository.saveAll(demographies);
-		
-		return demographies;
-	}
-
-	@GetMapping("findByPersonName/{personName}")
-	private List<Demography> create(@PathVariable String personName) {
-		return respository.findByPersonName(personName);
-	}
-
-
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-
-
 }
---
+++ src/main/java/com/example/demo/demography/api/Update.java
@@ -0,0 +1,4 @@
+package com.example.demo.demography.api;
+
+public class Update {
+}
